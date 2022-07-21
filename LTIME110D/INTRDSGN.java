5a19cbb6dc1b6e2799883fcf88975676f2adaf9c
Ayush kumar bharti <aayushkumar1011@gmail.com>
2022-07-21 13:01:19 +0530

    Interior Design
    https://www.codechef.com/LTIME110D/problems/INTRDSGN/

---
+++ LTIME110D/INTRDSGN.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		for(int t = 1; t <= test; t++){
+		    int x1 = sc.nextInt();
+		    int y1 = sc.nextInt();
+		    int x2 = sc.nextInt();
+		    int y2 = sc.nextInt();
+		    System.out.println(Math.min(x1+y1, x2+y2));
+		}
+	}
+}
